781da9da2bd0ba77865685a8125db7b4215dfd43
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2018-07-27 18:43:10 +0900

    3rd commit

--- get_commits.py
+++ get_commits.py
@@ -66,8 +66,10 @@ def test():
 
     web_hook = "https://hooks.slack.com/services/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"
 
+    message = "hello"
+
     requests.post(web_hook, data = json.dumps({
-        'text': u'新しいcommitはありません', # 投稿するテキスト
+        'text': message, # 投稿するテキスト
         'username': u'Ghost', # 投稿のユーザー名
         'icon_emoji': u':ghost:', # 投稿のプロフィール画像に入れる絵文字
         'link_names': 1, # メンションを有効にする
